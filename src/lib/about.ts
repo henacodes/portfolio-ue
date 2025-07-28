0dc7e3d781ff2febc525775b3c62565214141394
kirakos <henacodes@gmail.com>
2025-07-28 12:42:58 +0300

    slight update

---
+++ src/lib/about.ts
@@ -0,0 +1,6 @@
+export const me = {
+	name: 'Henok Ayenew',
+	alias: 'Kirakos',
+	tools: ['Svelte Kit', 'Next Js', 'Typescript', 'Supabse', 'ExpressJs'],
+	education: 'Undergraduate BSc, Software Engineering, AASTU'
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import { projects } from '$lib/projects';
 	import ProjectCard from '../components/ProjectCard.svelte';
+	import { me } from '$lib/about';
 </script>
 
 <img
 	src="/motif-bg.png"
-	class="motif fixed top-0 left-0 -z-10 h-[100vh] w-[100vw] object-cover"
+	class="motif fixed top-0 left-0 -z-10 h-[100vh] w-[100vw] scale-200 rotate-45 object-cover"
 	alt=""
 />
 
 <div class="app">
-	<div class=" flex h-[60vh] items-center p-10">
-		<div>
+	<div class="  h-[] flex h-[80vh] items-center justify-center p-10">
+		<div class=" ">
 			<img src="/247.png" alt="" />
 			<p
 				class="text-4xl font-extrabold text-[#72410D] sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl"
@@ -28,8 +29,20 @@
 				<img class=" h-6 w-6 grayscale" src="supabase.svg" alt="" srcset="" />
 			</div>
 		</div>
+
+		<div class=" bio-div relative z-10 flex h-full flex-col items-center justify-center">
+			<p class=" text-4xl font-semibold text-[#72410D]">{me.name}</p>
+			<p>{me.education}</p>
+
+			<p class=" mt-5 font-bold">Tools</p>
+			<ul class="flex list-disc">
+				{#each me.tools as tool}
+					<li class=" mx-4">{tool}</li>
+				{/each}
+			</ul>
+		</div>
 	</div>
-	<img src="/tribal-colors.png" class="bottom-0 left-0 mt-48 w-[100vw]" alt="" />
+	<img src="/tribal-colors.png" class=" mt-20 w-[100vw]" alt="" />
 	<div class="bg-[#72410D] py-14">
 		<p class=" m-5 text-center text-2xl text-[#fff3d2]">Projects</p>
 		<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
@@ -44,8 +57,8 @@
 
 <style>
 	.motif {
-		mask-image: linear-gradient(to left, black, transparent 70%);
-		-webkit-mask-image: linear-gradient(to left, black, transparent 70%);
+		mask-image: linear-gradient(to left, black, transparent 60%);
+		-webkit-mask-image: linear-gradient(to left, black, transparent 60%);
 	}
 
 	@media (max-width: 768px) {
@@ -53,5 +66,13 @@
 			mask-image: linear-gradient(to bottom, black, transparent 30%);
 			-webkit-mask-image: linear-gradient(to bottom, black, transparent 30%);
 		}
+	}
+
+	.bio-div {
+		width: 700px;
+		background-image: url('/papyrus.png');
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: cover;
 	}
 </style>
