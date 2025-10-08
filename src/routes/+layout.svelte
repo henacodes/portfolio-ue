3ec20eefa3b3a03a7583a9bbbd1b102c3bec9e31
kirakos <henacodes@gmail.com>
2025-10-08 07:45:12 +0300

    ...

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,12 +15,12 @@
 	});
 </script>
 
-<div
+<!-- <div
 	class=" fixed top-2 right-2 flex h-[5rem] w-[20rem] flex-col items-center justify-center rounded-lg bg-amber-500"
 	style={`${uiState['notif-1'] ? 'transform:translateY(0%)' : 'transform:translateY(-150%);'};` +
 		'transition: linear 0.3s;'}
 >
 	<p class=" text-amber-900">This page is an archaological finding</p>
 	<small class="  text-white">Source: Trust me bro</small>
-</div>
+</div> -->
 {@render children()}
